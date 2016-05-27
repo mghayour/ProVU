0637c86bc583f683434f7a00239408e5684c966c
MGhayour <me@mghayour.ir>
2016-05-27 21:29:43 +0430

    message board design

--- src/gui/MessageBoardController.java
+++ src/gui/MessageBoardController.java
@@ -9,6 +9,7 @@ package gui;
 
 //import javafx.css.StyleableObjectProperty
 //import com.fxexperience.javafx.animation.*;
+import com.jfoenix.controls.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -16,6 +17,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 
 import static gui.Helper.*;
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 //import gui.animation.*;
 
 /**
@@ -25,13 +33,47 @@ import static gui.Helper.*;
 public class MessageBoardController implements Initializable {
     
     @FXML private Pane rooter;
-
+    @FXML private VBox messageSubjectHolder;
+    
+    private StringProperty testStr = new SimpleStringProperty("T");
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         addBackground(rooter, 0.3, 1);
+        
+        ObservableList<Node> childs = messageSubjectHolder.getChildren();
+        HBox[] messageSubject = childs.toArray(new HBox[childs.size()]);
+        int id=-1;
+        for (HBox ms: messageSubject) {
+            id++;
+            
+            Label l = (Label)ms.getChildren().get(0);
+            if (l.getText().equals("پروژه سوم"))                    
+                l.textProperty().bind(testStr);
+            
+            Node c = ms.getChildren().get(1);
+            JFXButton b = c instanceof JFXButton ? (JFXButton)c : null ;
+            final int mid = id;
+            if (b!=null)
+                b.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        btn_viewPost_click_pro(mid);
+                    }
+                });
+            
+        }
+        
+        
     }
 
+    
+    @FXML void btn_viewPost_click() {
+        testStr.set(testStr.get()+"t");
+    }
+    @FXML void btn_viewPost_click_pro(int id) {
+        testStr.set(testStr.get()+id);
+    }
     
 
     
