900b4995635e8589456a3322d5a04a2514064d35
MGhayour <me@mghayour.ir>
2016-06-05 00:59:18 +0430

    first use of NameValue binder

--- src/gui/MessageBoardController.java
+++ src/gui/MessageBoardController.java
@@ -28,6 +28,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import logic.*;
 //import gui.animation.*;
 
 /**
@@ -38,28 +39,36 @@ public class MessageBoardController extends MyController {
     
     @FXML private Pane rooter;
     @FXML private VBox messageSubjectHolder;
+    ModelControlCollection messages;
+    NameValue currentPost, currentCourse;
+
+    @Override
+    public NameValue toNameValue() {
+        if (myNameValue==null) {
+            myNameValue = super.toNameValue();
+            
+            currentPost= new NameValue();
+            currentPost.bind(Post.toEmptyNameValue());
+            myNameValue.put("currentPost",currentPost);
+            
+            currentCourse= new NameValue();
+            currentCourse.bind(Course.toEmptyNameValue());
+            myNameValue.put("currentCourse",currentCourse);
+        }
+        return myNameValue;
+    }
     
-    private StringProperty testStr = new SimpleStringProperty("T");
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         super.initialize(url, rb);
         
-        
-        messageSubjectHolder.getChildren().clear();
-        
-        ModelControlCollection messages = new ModelControlCollection(messageSubjectHolder, "model/messageItemInMessageBoard.fxml") {
+       
+        messages = new ModelControlCollection(messageSubjectHolder, "model/messageItemInMessageBoard.fxml") {
             @Override
             public void onButtonClick (NameValue data) {
                 StringProperty number = data.getStringProperty("number");
                 int n = data.getInt("number") + 1;
-                number.set(""+n);
-            }
-            
-            @Override
-            public void onRootClick (NameValue data) {
-                StringProperty number = data.getStringProperty("number");
-                int n = data.getInt("number") - 1;
                 number.set(""+n);
             }
         };
@@ -75,72 +84,13 @@ public class MessageBoardController extends MyController {
         }
         for (int i=0; i<100; i+=2)
             messages.remove(i);
-                    
-        /*
-        ObservableList<Node> childs = messageSubjectHolder.getChildren();
-        
-        childs.clear();
-        //ModelControl message = new ModelControl("model/messageItemInMessageBoard.fxml");
-        PatternControl message = new PatternControl("model/messageItemInMessageBoard.fxml") {
-            @Override
-            public void onButtonClick (NameValue data) {
-                StringProperty number = data.getStringProperty("number");
-                int n = data.getInt("number") + 1;
-                number.set(""+n);
-            }
-            
-            @Override
-            public void onRootClick (NameValue data) {
-                StringProperty number = data.getStringProperty("number");
-                int n = data.getInt("number") - 1;
-                number.set(""+n);
-            }
-        };
-        
-        NameValue data = new NameValue();
-        data.put("title",new SimpleStringProperty("کوئیز جاوا"));
-        data.put("show",new SimpleStringProperty("مشاهده"));
-        data.put("number",new SimpleStringProperty("5"));
 
-        //for (int i=0; i <100; i++)
-        Node n = message.generate(data);
-        childs.add(n);
-        //childs.add(n);
-        
-        childs.remove(n);
-        */
-        /*
-        HBox[] messageSubject = childs.toArray(new HBox[childs.size()]);
-        int id=-1;
-        for (HBox ms: messageSubject) {
-            id++;
-            
-            Label l = (Label)ms.getChildren().get(0);
-            if (l.getText().equals("پروژه سوم"))                    
-                l.textProperty().bind(testStr);
-            
-            Node c = ms.getChildren().get(1);
-            JFXButton b = c instanceof JFXButton ? (JFXButton)c : null ;
-            final int mid = id;
-            if (b!=null)
-                b.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        btn_viewPost_click_pro(mid);
-                    }
-                });
-            
-        }
-        */
-        
     }
 
     
     @FXML void btn_viewPost_click() {
-        testStr.set(testStr.get()+"t");
     }
     @FXML void btn_viewPost_click_pro(int id) {
-        testStr.set(testStr.get()+id);
     }
     
 
